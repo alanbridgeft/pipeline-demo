e4db6de9b5c41f4404f3d177afae967d7c00a251
Alan Illing <alan@bridgeft.com>
2023-04-18 22:19:38 -0400

    Add error to return signature

--- lambda/main.go
+++ lambda/main.go
@@ -12,9 +12,9 @@ type Event struct {
 
 // handle takes a json of two numbers, like this: {"x": 12, "y": 8}
 // and returns a string with the sum, like this: sum=20
-func handle(event Event) string {
+func handle(event Event) (string, error) {
 	sum := event.X + event.Y
-	return fmt.Sprintf("sum=%d", sum)
+	return fmt.Sprintf("sum=%d", sum), nil
 }
 
 func main() {
